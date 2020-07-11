88c9d3e563a28b5c25bf931677dcc8e791ace654
kambojgurpreet <kambojgurpreet32@gmail.com>
2020-07-11 13:33:18 +0530

    added create functionality

--- app.js
+++ app.js
@@ -7,6 +7,30 @@ const ui = new UI();
 //show post on DOM load
 document.addEventListener('DOMContentLoaded', getPosts());
 
+//submit post
+const postSubmit = document.getElementById('post-submit');
+
+//add Event Listener
+postSubmit.addEventListener('click', e => {
+    //get input values
+    const postTitle = document.getElementById('post-title').value;
+    const postBody = document.getElementById('post-body').value;
+
+    const post = {
+        title: postTitle,
+        body: postBody
+    }
+
+    //create post
+    http.post('http://localhost:3000/posts', post)
+    .then((result) => {
+        console.log('POSTED');
+    }).catch((err) => {
+        console.log(err);
+    });
+
+    e.preventDefault();
+})
 
 //getPosts function
 function getPosts() {
